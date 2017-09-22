7478a95e3b299a00c5afa999316595799df9436d
Yihau-Chen <a122092487@gmail.com>
2017-09-22 17:43:57 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+
+//======================================================
+//
+//  main.cpp
+//
+//  Created by yihau [2017]
+//
+//======================================================
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
