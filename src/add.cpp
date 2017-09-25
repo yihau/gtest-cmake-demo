4c83b0458a7503185abe8ff019964b61f8081590
Yihau-Chen <a122092487@gmail.com>
2017-09-25 13:13:09 +0800

    add simple method - add

--- src/add.cpp
+++ src/add.cpp
@@ -1,15 +1,12 @@
 //======================================================
 //
-//  main.cpp
+//  add.cpp
 //
 //  Copyright [2017] yihau
 //
 //======================================================
 
-#include <iostream>
 #include "./add.h"
-
-int main(int argc, char** argv) {
-    std::cout << "1 + 2 = " << add(1, 2) << std::endl;
-    return 0;
+int add(int i, int j) {
+  return (i+j);
 }
